afd46750d9f293acf133b6533aa19587befc200c
Nicole Ho <nicole.ho@mail.mcgill.ca>
2017-11-29 19:16:15 -0500

    updated db

--- tableof6.sql
+++ tableof6.sql
@@ -1,114 +1,150 @@
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: tableof6
--- ------------------------------------------------------
--- Server version	5.5.5-10.1.28-MariaDB
+-- Host: localhost
+-- Generation Time: Nov 30, 2017 at 01:06 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tableof6`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `eventreg`
 --
 
-DROP TABLE IF EXISTS `eventreg`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `eventreg` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL,
   `Date` datetime NOT NULL,
   `Location` int(11) DEFAULT NULL,
-  `user1` varchar(30) NOT NULL,
-  `user2` varchar(30) DEFAULT NULL,
-  `user3` varchar(30) DEFAULT NULL,
-  `user4` varchar(30) DEFAULT NULL,
-  `user5` varchar(30) DEFAULT NULL,
-  `user6` varchar(30) DEFAULT NULL,
-  `NumRegistered` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `user1` int(11) DEFAULT NULL,
+  `user2` int(11) DEFAULT NULL,
+  `user3` int(11) DEFAULT NULL,
+  `user4` int(11) DEFAULT NULL,
+  `user5` int(11) DEFAULT NULL,
+  `user6` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `eventreg`
 --
 
-LOCK TABLES `eventreg` WRITE;
-/*!40000 ALTER TABLE `eventreg` DISABLE KEYS */;
-INSERT INTO `eventreg` VALUES (1,'2017-12-05 12:00:00',1,'1','2','3','17',NULL,NULL,3),(2,'2017-11-30 15:00:00',4,'1','2','3',NULL,NULL,NULL,3),(3,'2017-12-04 17:30:00',2,'2','3',NULL,NULL,NULL,NULL,2),(4,'2017-12-03 21:00:00',5,'3','5','8','12','2','1',6),(5,'2017-12-02 15:00:00',5,'6','','','',NULL,NULL,1);
-/*!40000 ALTER TABLE `eventreg` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `eventreg` (`ID`, `Date`, `Location`, `user1`, `user2`, `user3`, `user4`, `user5`, `user6`) VALUES
+(1, '2017-12-05 12:00:00', 1, 3, 2, 5, 17, NULL, NULL),
+(2, '2017-11-30 15:00:00', 4, 1, 2, NULL, NULL, NULL, NULL),
+(3, '2017-12-04 17:30:00', 2, 2, 3, 1, NULL, NULL, NULL),
+(4, '2017-12-03 21:00:00', 5, 3, 5, 8, 12, 2, 1),
+(5, '2017-12-02 15:00:00', 5, 6, 5, 1, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `restaurants`
 --
 
-DROP TABLE IF EXISTS `restaurants`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `restaurants` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `address` varchar(45) NOT NULL,
-  `number` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `number` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `restaurants`
 --
 
-LOCK TABLES `restaurants` WRITE;
-/*!40000 ALTER TABLE `restaurants` DISABLE KEYS */;
-INSERT INTO `restaurants` VALUES (1,'Bouillon Bilk','1234 rue sth','2147483647'),(2,'restaurant2','4935 rue sth','5144928573'),(3,'Kazu','4134 rue sth','4388620283'),(4,'onoir','1234 rue maisonneuve','4387210283'),(5,'resutarant1','1982 rue sth','4389608492');
-/*!40000 ALTER TABLE `restaurants` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `restaurants` (`ID`, `name`, `address`, `number`) VALUES
+(1, 'Bouillon Bilk', '1234 rue sth', '2147483647'),
+(2, 'restaurant2', '4935 rue sth', '5144928573'),
+(3, 'Kazu', '4134 rue sth', '4388620283'),
+(4, 'onoir', '1234 rue maisonneuve', '4387210283'),
+(5, 'resutarant1', '1982 rue sth', '4389608492');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
   `userName` varchar(30) NOT NULL,
   `userEmail` varchar(60) NOT NULL,
-  `userPass` varchar(255) NOT NULL,
-  PRIMARY KEY (`userId`),
-  UNIQUE KEY `userEmail` (`userEmail`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `userPass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'joyce','joyce@123.com','96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e'),(2,'judith','judith@123.com','96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e'),(3,'nicole','nicole@123.com','96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` (`userId`, `userName`, `userEmail`, `userPass`) VALUES
+(1, 'joyce', 'joyce@123.com', '96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e'),
+(2, 'judith', 'judith@123.com', '96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e'),
+(3, 'nicole', 'nicole@123.com', '96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `eventreg`
+--
+ALTER TABLE `eventreg`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `ID_UNIQUE` (`ID`);
+
+--
+-- Indexes for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `ID_UNIQUE` (`ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`),
+  ADD UNIQUE KEY `userEmail` (`userEmail`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `eventreg`
+--
+ALTER TABLE `eventreg`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-11-29 12:48:43
