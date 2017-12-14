788b0c285e279b1713b9b079bfb9e5bf30b5630e
judith77 <33427977+judith77@users.noreply.github.com>
2017-12-14 13:48:49 -0500

    Add files via upload

--- pyth_address.py
+++ pyth_address.py
@@ -21,38 +21,69 @@ for each in range(0, len(response)):
 #print response1[0]
 #print final_list[0]
 
-#resto 1 
 url = 'https://maps.googleapis.com/maps/api/geocode/json'
-params = {'sensor': 'false', 'address': response[0]}
-r = requests.get(url, params=params)
-results = r.json()['results']
-location = results[0]['geometry']['location']
-latti = location['lat']
-lotte = location['lng']
+latti, latti2, latti3, latti4 = (0,0,0,0)
+lotte, lotte2, lotte3, lotte4 = (0,0,0,0)
+
+#resto 1 
+def resto1():
+	params = {'sensor': 'false', 'address': response[0]}
+	r = requests.get(url, params=params)
+	results = r.json()['results']
+	if results:
+		location = results[0]['geometry']['location']
+		global latti
+		global lotte
+		latti = location['lat']
+		lotte = location['lng']
+	else: 
+		resto1()
 
 #resto2 
-params2 = {'sensor': 'false', 'address': response[1]}
-r2 = requests.get(url, params=params2)
-results2 = r2.json()['results']
-location2 = results2[0]['geometry']['location']
-latti2 = location2['lat']
-lotte2 = location2['lng']
+def resto2():
+	params2 = {'sensor': 'false', 'address': response[1]}
+	r2 = requests.get(url, params=params2)
+	results2 = r2.json()['results']
+	if results2:
+		location2 = results2[0]['geometry']['location']
+		global latti2
+		global lotte2
+		latti2 = location2['lat']
+		lotte2 = location2['lng']
+	else: 
+		resto2()
 
 #resto3 
-params3 = {'sensor': 'false', 'address': response[2]}
-r3 = requests.get(url, params=params3)
-results3 = r3.json()['results']
-location3 = results3[0]['geometry']['location']
-latti3 = location3['lat']
-lotte3 = location3['lng']
+def resto3():
+	params3 = {'sensor': 'false', 'address': response[2]}
+	r3 = requests.get(url, params=params3)
+	results3 = r3.json()['results']
+	if results3:
+		location3 = results3[0]['geometry']['location']
+		global latti3
+		global lotte3
+		latti3 = location3['lat']
+		lotte3 = location3['lng']
+	else: 
+		resto3()
 
 #resto4 
-params4 = {'sensor': 'false', 'address': response[3]}
-r4 = requests.get(url, params=params4)
-results4 = r4.json()['results']
-location4 = results4[0]['geometry']['location']
-latti4 = location4['lat']
-lotte4 = location4['lng']
+def resto4():
+	params4 = {'sensor': 'false', 'address': response[3]}
+	r4 = requests.get(url, params=params4)
+	results4 = r4.json()['results']
+	if results4: 
+		location4 = results4[0]['geometry']['location']
+		global latti4
+		global lotte4
+		latti4 = location4['lat']
+		lotte4 = location4['lng']
+	else:
+		resto4()
 
+resto1()
+resto2()
+resto3()
+resto4()
 print(latti, lotte, latti2, lotte2, latti3, lotte3, latti4, lotte4)
 
